4e0adaa2a902e3912a5548c1f11a12e499277e21
ruisimoes <ruisimoes@ua.pt>
2015-12-10 23:26:52 +0000

    Re-Configuration

---
+++ app/code/bootstrap.ts
@@ -0,0 +1,6 @@
+/// <reference path="../../typings/angularjs/angular.d.ts" />
+/// <reference path="../../typings/angularjs/angular-route.d.ts" />
+
+'use strict';
+
+angular.module('bootstrap', ['ngRoute']);
